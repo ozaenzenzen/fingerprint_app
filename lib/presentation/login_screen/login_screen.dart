2d0dbe0f4d53c312dc0ebc8a896b9986b5f6075a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-08-13 15:46:23 +0700

    update: button color popup

--- lib/presentation/login_screen/login_screen.dart
+++ lib/presentation/login_screen/login_screen.dart
@@ -216,6 +216,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                     mainButtonAction: () {
                                       Get.back();
                                     },
+                                    mainButtonColor: const Color(0xff1183FF),
                                   );
                                 },
                               );
